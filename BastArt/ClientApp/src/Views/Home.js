0669ece2f3a8779661c37ca9fe8584f64a41b06a
Baraciak <43997064+Baraciak@users.noreply.github.com>
2019-12-05 12:09:27 +0100

    react structure/skeleton refactored/ base ap lets's start

---
+++ BastArt/ClientApp/src/Views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+		<div id="home-page">
+			<h1>Hello, world!</h1>
+		</div>
+    );
+  }
+}
