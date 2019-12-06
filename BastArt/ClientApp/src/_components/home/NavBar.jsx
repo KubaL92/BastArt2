90f1d5fa59f3283abb1d9b2e466806c7a6a964b0
Baraciak <43997064+Baraciak@users.noreply.github.com>
2019-12-06 13:51:51 +0100

    home css fixes

--- BastArt/ClientApp/src/_components/home/NavBar.jsx
+++ BastArt/ClientApp/src/_components/home/NavBar.jsx
@@ -4,16 +4,31 @@ import React, { Component } from 'react'
 class NavBar extends Component{
     render(){
         return(
-            <nav className="navbar sticky-top navbar-expand-lg navbar-dark mt-2">
+            <nav className="navbar sticky-top navbar-expand-lg navbar-dark" id="navig">
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <button className="btn nav-item nav-link">Home</button>
-                        <button className="btn nav-item nav-link">Tutorial</button>
-                        <button className="btn nav-item nav-link">Artworks</button>
-                        <button className="btn nav-item nav-link">About</button>
+                    <div className="navbar-nav" id="nav-buttons">
+                        <button className="ml-4 nav-item nav-link">Home</button>
+                        <button className="ml-4 nav-item nav-link">Tutorial</button>
+                        <button className="ml-4 nav-item nav-link">Artworks</button>
+                        <button className="ml-4 nav-item nav-link">About</button>
                     </div>
                 </div>
             </nav>
+
+            // <ul class="nav justify-content-center navbar">
+            //     <li class="nav-item">
+            //         <a class="nav-link" href="#">Active</a>
+            //     </li>
+            //     <li class="nav-item">
+            //         <a class="nav-link" href="#">Link</a>
+            //     </li>
+            //     <li class="nav-item">
+            //         <a class="nav-link" href="#">Link</a>
+            //     </li>
+            //     <li class="nav-item">
+            //         <a class="nav-link disabled" href="#">Disabled</a>
+            //     </li>
+            // </ul>
         )
     }
 
