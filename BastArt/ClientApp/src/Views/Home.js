ef0372deff2b6c1c92f89ef5c02b45a5c57af4af
KubaL92 <krzysztof.lomber92@gmail.com>
2019-12-06 01:14:35 +0100

    added new screens and css

--- BastArt/ClientApp/src/Views/Home.js
+++ BastArt/ClientApp/src/Views/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NavBar from '../_components/home/NavBar';
 import ArtworksScreen from '../_components/home/ArtworksScreen';
+import HomeScreen from '../_components/home/HomeScreen';
+import AboutScreen from '../_components/home/AboutScreen';
+import Tutorial from '../_components/home/TutorialScreen';
 import '../_static/css/home.css';
 
 export class Home extends Component {
@@ -9,8 +12,11 @@ export class Home extends Component {
   render () {
     return (
 		<div id="home-page" className="container">
-			<NavBar />
-			<ArtworksScreen />
+            <NavBar />
+            <HomeScreen />
+            <ArtworksScreen />
+            <Tutorial />
+            <AboutScreen />
 		</div>
     );
   }
--- BastArt/ClientApp/src/_components/home/NavBar.jsx
+++ BastArt/ClientApp/src/_components/home/NavBar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-// import '../../_static/css/navbar.css';
+//import '../../_static/css/navbar.css';
 
 class NavBar extends Component{
     render(){
         return(
             <nav className="navbar sticky-top navbar-expand-lg navbar-dark mt-2">
-                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <button className="btn nav-item nav-link">Home</button>
                         <button className="btn nav-item nav-link">Tutorial</button>
