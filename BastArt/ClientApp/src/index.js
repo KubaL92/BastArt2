9e59c103333f5e4c496c08cbedac2b955f1cce50
Baraciak <43997064+Baraciak@users.noreply.github.com>
2019-12-19 11:55:55 +0100

    merged newProjWeb with portfolio

--- BastArt/ClientApp/src/index.js
+++ BastArt/ClientApp/src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bs-stepper/dist/css/bs-stepper.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
