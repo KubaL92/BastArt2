04d71766634fee250239e5d295adcff14c3a765d
Baraciak <43997064+Baraciak@users.noreply.github.com>
2020-01-14 20:16:47 +0100

    nie wiem ale dodam

---
+++ BastArt/ClientApp/src/Views/Portfolio.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import { ProfileHeader } from '../_components/portfolio/ProfileHeader'
+import { Artworks } from '../_components/portfolio/Artworks'
+import { Navigation } from '../_components/portfolio/Navigation'
+import {About} from '../_components/portfolio/About'
+
+import {getByUserId} from '../_helpers/portfoliosApi';
+import {connect} from 'react-redux';
+import TagBox from '../_components/portfolio/TagBox';
+import styled from 'styled-components';
+
+
+const PortfolioContainer = styled.div`
+    min-height: 600px;
+` 
+
+
+class Portfolio extends Component {
+    state={
+        portfolioFetched: false,
+        owner: false,
+        portfolio: {},
+        // tags: [],
+        show: 'Artworks'
+    }
+    
+    componentDidMount() {
+        const {id, currentUser} = this.props;
+        if(currentUser !== null){
+            if(id === currentUser.id)
+            this.setState({owner: true});
+        }
+        this.fetchPortfolio(id);
+    }
+    
+    onClickAbout = () =>{
+        this.setState({show: 'About'});
+    }
+
+    onClickArtworks = () =>{
+        this.setState({show: 'Artworks'});
+    }
+
+    render(){
+        const {portfolioFetched, show, portfolio} = this.state;
+        return (
+            <React.Fragment>
+                <div className="screen">
+                    {portfolioFetched
+                        ?<ProfileHeader user={portfolio.user}/>
+                        :<p>Loading...</p>
+                    }
+                </div>
+                <div className="screen">
+                    {portfolioFetched
+                        ?<TagBox tags={portfolio.tags}/>
+                        :<p>Loading...</p>
+                    }
+                </div>
+                <div className="screen">
+                    <PortfolioContainer>
+                        <Navigation onClickAbout={this.onClickAbout} onClickArtworks={this.onClickArtworks} />
+                        <hr />
+                        {portfolioFetched
+                            ?   (
+                                    show === 'Artworks'
+                                    ?<Artworks graphics={portfolio.graphics}/>
+                                    :<About  about={portfolio.user.profile.about} style={{minHeight: "100"}}/>
+                                )
+                            :<p>Loading...</p>
+                        }
+                    </PortfolioContainer>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+    fetchPortfolio = async (id) =>{
+        const portfolioData = await getByUserId(id);
+        console.log(portfolioData, "portfolio");
+        this.setState({
+            portfolio: portfolioData,
+            portfolioFetched: true
+        });
+    } 
+}
+
+const mapStateToProps = (state) => ({
+    currentUser: state.currentUser,
+    role: state.role
+})
+
+export default connect(mapStateToProps, {}) (Portfolio);
